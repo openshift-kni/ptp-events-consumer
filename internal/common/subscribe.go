1110c8af77175bf3886dedb99e60e87f2443bbec
jenchen <jenchen@redhat.com>
2024-07-03 17:00:47 +0000

    subscribe ptp state change only

--- internal/common/subscribe.go
+++ internal/common/subscribe.go
@@ -14,9 +14,9 @@ import (
 // GetResources ... get resource types
 func GetResources() map[string]string {
 	subscribeTo := map[string]string{}
-	subscribeTo[string(ptpEvent.OsClockSyncStateChange)] = string(ptpEvent.OsClockSyncState)
+	//subscribeTo[string(ptpEvent.OsClockSyncStateChange)] = string(ptpEvent.OsClockSyncState)
 	subscribeTo[string(ptpEvent.PtpStateChange)] = string(ptpEvent.PtpLockState)
-	subscribeTo[string(ptpEvent.PtpClockClassChange)] = string(ptpEvent.PtpClockClass)
+	//subscribeTo[string(ptpEvent.PtpClockClassChange)] = string(ptpEvent.PtpClockClass)
 	return subscribeTo
 }
 
